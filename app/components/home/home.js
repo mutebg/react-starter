31ee3230f1ede80bad69aa20daf125c90184e061
Stoyan Delev <s.delev@elements.nl>
2016-02-21 19:39:35 +0100

    add test framework

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -5,9 +5,18 @@ import React from 'react';
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+        count: 0
+    }
+
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnClick() {
+      this.setState({count: this.state.count + 1});
   }
 
   render() {
-    return <div>Home</div>;
+    return <div className="home" onClick={this.handleOnClick}>Counter: {this.state.count}</div>;
   }
 }
---
+++ app/components/home/home_test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Home from './home';
+
+describe("Home Component", () => {
+
+  it("contains class .home", () => {
+      expect(shallow(<Home />).is('.home')).toBe(true);
+  });
+
+  it("simulate click and count", () => {
+      const wrapper = shallow(<Home />);
+      wrapper.find('div').simulate('click');
+      expect( wrapper.text() ).toEqual('Counter: 1');
+  });
+
+});
